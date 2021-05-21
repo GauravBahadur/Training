2285b83b2573569a4b7489603924779d9455f93e
Gaurav Bahadur <Gauravbahadur1997@gmail.com>
2021-05-21 16:17:14 +0530

    Add files via upload

---
+++ Program5.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Program5 {
+	public static void main(String[] args)throws IOException {
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Enter a number to check whether it's prime or not! ");
+		int n=Integer.parseInt(br.readLine());
+		try {
+			if(n>100)
+			{
+				throw new ArithmeticException("Number should have been less than 100!");
+			}
+			if(n<=0) {
+				throw new ArithmeticException("Number should have been greater than 0");
+
+			}
+			else {
+				if(n==1)
+					System.out.println(n+ " is not a prime number!");
+				else
+				{
+					int i;boolean b=true;
+					for(i=2;i*i<=n;i++)
+					{
+						if(n%i==0) {
+							b=false;
+							break;
+						}
+					}
+					System.out.println(b?" number entered is prime ":"number entered is not prime");
+				}
+			}
+					
+					
+		}
+		catch(ArithmeticException e)
+		{
+			System.out.println("Some exception "+e);
+		}
+	}
+}
